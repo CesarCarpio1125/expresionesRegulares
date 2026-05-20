731b76fe6edb45bb0efce6b5c9115503bd83c049
Elias <elias@Cesar-MacBook-Pro.local>
2026-05-19 22:52:00 -0500

    Eager loading fix - todo en un solo JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,21 @@
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 
+const pages = import.meta.glob('./pages/*.vue', { eager: true });
+
 createInertiaApp({
   resolve: (name) => {
-    const pages = import.meta.glob('./pages/*.vue', { eager: true });
     const page = pages[`./pages/${name}.vue`];
-    
     if (!page) {
       throw new Error(`Page not found: ${name}`);
     }
-    
     return page;
   },
   setup({ el, App, props, plugin }) {
-    const app = createApp({ render: () => h(App, props) });
-    app.use(plugin);
-    app.mount(el);
+    createApp({
+      render: () => h(App, props),
+    })
+    .use(plugin)
+    .mount(el);
   },
 });
